675294f704cc3c64ed0d6c118da8f616353a1fda
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-07-09 19:31:18 +0530

    HC: L4 => Creating a custom react (4.1)

--- custom_react_01/custom_react.js
+++ custom_react_01/custom_react.js
@@ -1,4 +1,24 @@
+function customRender(reactElement, mainContainer){
+    const domElement = document.createElement(reactElement.type)
+    domElement.append(reactElement.children)
+    // let props_arr = reactElement.props.getAttributeNames()
+    let props_arr = Object.keys(reactElement.props)
+    props_arr.forEach((property)=>{
+        domElement.setAttribute(property, reactElement.props[property])
+    })
+    
+    mainContainer.appendChild(domElement)
+}
+
 const reactElement = {
-    type:
+    type: 'a',
+    props:{
+        href:'https://google.com',
+        target: '_blank'
+    }, 
+    children: 'Click me to visit google'
 };
-let mainContainer = document.getelementById('root');
+
+let mainContainer = document.getElementById('root');
+
+customRender(reactElement, mainContainer)
