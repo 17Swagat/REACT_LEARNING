04b63875172bb4f60c99c612b6b0e47aa2f741b3
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-07-09 18:19:37 +0530

    HC: L3=> Basic Manipulation 1.2 Adding a Tea Component pt - 2

--- basics_01/src/main.jsx
+++ basics_01/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Tea from './Chai.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -8,3 +9,10 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 );
+
+
+createRoot(document.querySelector('#root2')).render(
+  <StrictMode>
+    <Tea/>
+  </StrictMode>
+)
