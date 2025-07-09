833bf1b2ea39c4ff08150211100af1b8a570b42b
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-07-10 01:24:38 +0530

    HC: L4 => Passing multiple content elements to .render() function which are created via React.createElement(...) function

--- basics_01/src/main.jsx
+++ basics_01/src/main.jsx
@@ -1,43 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Tea from './Chai.jsx'
+import React from 'react'
 
 
-// function Div_withText(){
-//   return (
-//     <>
-//     <p>Abolestiae. Libero provident dignissimos sunt pariatur fuga rem aut reprehenderit quas! Fuga dolor asperiores nihil magnam corrupti quae? Vel sequi, iusto officiis nisi eius deserunt blanditiis! Quae eos cupiditate accusamus a repudiandae. Alias!</p>
-//     </>
-//   );
-// }
+let element = document.getElementById('root')
+element.style.height = '300px'
+element.style.backgroundColor = 'rgb(122, 39, 39)'
+element.style.paddingLeft = '10px'
+element.style.paddingTop = '1px'
 
 
-// let div_root1 = document.getElementById('root1')
-// div_root1.style.backgroundColor = 'yellow';
-// div_root1.style.height = '200px'
-// div_root1.style.marginBottom = '30px'
-// div_root1.style.padding = '10px'
-// div_root1.style.fontSize = '35px'
-// div_root1.style.borderRadius = '20px'
-
-// createRoot(div_root1).render(
-//   <>
-//   <Div_withText/>
-//   </>
-// )
+let element_Content_1 = React.createElement('h1', null, 'Luny Tunes')
+let element_Content_2 = React.createElement('p', null, 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
 
 
-let a_link_Content = (
+createRoot(element).render(
   <>
-  <a href="https://google.com" target='_blank' style={{backgroundColor:'blue', fontSize:'38px', color:'black', padding:'10px', borderRadius:'20px', textDecoration: 'underline'}}>
-    LET'S GO TO GOOGLE
-  </a>
+  {element_Content_1}
+  {element_Content_2}
   </>
-);
-
-
-let a_link = document.getElementById('a_link')
-createRoot(a_link).render(
-  a_link_Content
-);
+)
