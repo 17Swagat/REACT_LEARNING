d62ad7a3e9049968c13551f4e6492029402af51d
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-07-09 18:12:15 +0530

    HC: L3=> Basic Manipulation

--- basics_01/src/App.jsx
+++ basics_01/src/App.jsx
@@ -2,10 +2,12 @@ import Coffee  from "./Coffee";
 function App() {
   return (
     //<h1>xReact with VITE</h1>
-    
     <>
     <Coffee />
+    <hr />
     <h1>Hello World</h1>
+    <hr />
+    <p>Learning React</p>
     </>
   )
 }
