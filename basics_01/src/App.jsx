9cb322504d8d2b558db0a46bece4ce54d197054f
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-07-10 15:40:10 +0530

    HC: L4 => evaluated expressions { }

--- basics_01/src/App.jsx
+++ basics_01/src/App.jsx
@@ -2,9 +2,18 @@ import Tea from "./Chai";
 import Coffee  from "./Coffee";
 
 function App() {
+
+ // JS code:
+ let user_name = 'Luny Tunes';
+
   return (
     <>
+    <h1 style={{textDecoration: 'underline', color:'yellow'}}>{user_name}</h1>
 
+    <div style={{backgroundColor: 'yellow', color: 'rgb(10, 73, 168)', padding: '3px', paddingLeft: '10px', paddingRight: '10px', fontSize: '24px', fontWeight: 'bold'}}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ea veritatis doloribus ab impedit ut rerum, fugiat accusantium optio molestiae at hic nihil. Labore, magnam cum fuga asperiores alias accusamus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ea veritatis doloribus ab impedit ut rerum, fugiat accusantium optio molestiae at hic nihil. Labore, magnam cum fuga asperiores alias accusamus.</p>
+    </div>
     </>
   )
 }
--- basics_01/src/main.jsx
+++ basics_01/src/main.jsx
@@ -1,32 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
+import App from './App.jsx'
 
 
-let element = document.getElementById('root')
-element.style.height = '300px'
-element.style.backgroundColor = 'rgb(122, 39, 39)'
-element.style.paddingLeft = '10px'
-element.style.paddingTop = '1px'
-
-
-let element_Content_1 = React.createElement('h1', 
-  {'style':{'textDecoration': 'underline', color: 'pink'}}, 
-  'Luny Tunes')
-
-let element_Content_2 = React.createElement(
-  'p',
-  {style: {color: 'white', fontSize: '26px'}}, 
-  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat molestias architecto minima non porro impedit modi maxime assumenda atque, eos error? Sapiente animi dolore illo. Sapiente delectus nam eos vitae.')
-
-  
-let element_Content_3 = React.createElement('a', {href:'https://google.com', target:'_blank', style:{fontWeight: 'bolder', color: 'rgb(141, 23, 23)', marginTop: '25px', display: 'inline-block', fontSize: '30px', backgroundColor: 'yellow'}}, 'CLICK TO GET FLICK')
-
-createRoot(element).render(
-  <>
-  {element_Content_1}
-  {element_Content_2}
-  {element_Content_3}
-  {}
-  </>
+createRoot(document.getElementById('root')).render(
+<App/>
 )
+
