eec4239f4f505f402d8841784106af6201abed5b
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-07-10 15:12:58 +0530

    HC: L4 => encountrering display property in css

--- basics_01/src/main.jsx
+++ basics_01/src/main.jsx
@@ -14,13 +14,19 @@ let element_Content_1 = React.createElement('h1',
   {'style':{'textDecoration': 'underline', color: 'pink'}}, 
   'Luny Tunes')
 
-let element_Content_2 = React.createElement('p', 
-  null, 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+let element_Content_2 = React.createElement(
+  'p',
+  {style: {color: 'white', fontSize: '26px'}}, 
+  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat molestias architecto minima non porro impedit modi maxime assumenda atque, eos error? Sapiente animi dolore illo. Sapiente delectus nam eos vitae.')
 
+  
+let element_Content_3 = React.createElement('a', {href:'https://google.com', target:'_blank', style:{fontWeight: 'bolder', color: 'rgb(141, 23, 23)', marginTop: '25px', display: 'inline-block', fontSize: '30px', backgroundColor: 'yellow'}}, 'CLICK TO GET FLICK')
 
 createRoot(element).render(
   <>
   {element_Content_1}
   {element_Content_2}
+  {element_Content_3}
+  {}
   </>
 )
