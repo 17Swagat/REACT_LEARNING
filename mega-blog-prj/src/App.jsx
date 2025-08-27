90e07d3518236abb94b256c3dea0b10f51d310b5
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-08-27 17:50:24 +0530

    MegaBlog -- Starting

---
+++ mega-blog-prj/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-black text-white">
+      <h1 className="text-9xl">hello World</h1>
+
+    </div>
+  )
+}
+
+export default App
